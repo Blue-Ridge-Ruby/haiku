8a2bb168f1c491f00344b57fc52f2d48fafce365
Jeremy Smith <jeremy@jeremysmith.co>
2026-03-20 13:21:06 -0400

    Redesign

--- test/system/home_test.rb
+++ test/system/home_test.rb
@@ -4,7 +4,7 @@ class HomeTest < ApplicationSystemTestCase
   test "home page displays" do
     visit root_url
 
-    assert_text "Blue Ridge Ruby Haiku Giveaway"
+    assert_text "Haiku Ticket Giveaway"
     assert_text "Submit Your Haiku"
     assert_text "0 haikus submitted"
   end
