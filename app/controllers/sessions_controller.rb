5e82c4c1f32fab757eb4d16e9eb47197faa3b8be
Jeremy Smith <jeremy@jeremysmith.co>
2026-03-22 21:39:45 -0400

    Tweaks (#7)
    * Homepage layout tweaks
    
    * Remove redundancy in before_actions
    
    * Refactor find_or_create_from_omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_or_create_from_omniauth(auth)
+    user = User.find_or_create_from_omniauth!(auth)
     session[:user_id] = user.id
-    redirect_to root_path, notice: "Signed in!"
+    redirect_to root_path, notice: "Logged in!"
   end
 
   def destroy
