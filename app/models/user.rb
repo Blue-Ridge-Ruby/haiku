313bdf598360e131c8c0cf3ff4539e5583d66b27
Jeremy Smith <jeremy@jeremysmith.co>
2026-03-22 21:38:09 -0400

    Refactor find_or_create_from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,12 @@ class User < ApplicationRecord
   has_one :submission
   has_many :votes
 
-  def self.find_or_create_from_omniauth(auth)
-    find_or_create_by(github_uid: auth.uid) do |user|
-      user.github_username = auth.info.nickname
-      user.github_email = auth.info.email
-    end.tap do |user|
-      user.update(
-        github_username: auth.info.nickname,
-        github_email: auth.info.email
-      )
-    end
+  def self.find_or_create_from_omniauth!(auth)
+    user = find_or_initialize_by(github_uid: auth.uid)
+    user.update!(
+      github_username: auth.info.nickname,
+      github_email: auth.info.email
+    )
+    user
   end
 end
