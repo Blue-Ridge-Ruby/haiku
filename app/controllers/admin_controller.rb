7b72681566fb0aba35c52d72d095423a9de602a2
Jeremy Smith <jeremy@jeremysmith.co>
2026-03-22 21:56:04 -0400

    Add export page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,21 @@ class AdminController < ApplicationController
     @submissions = Submission.includes(:user, :votes).order(created_at: :desc)
   end
 
+  def export
+    submissions = Submission.kept.includes(:user, :votes).map do |submission|
+      {
+        github_username: submission.user.github_username,
+        github_email: submission.user.github_email,
+        line1: submission.line1,
+        line2: submission.line2,
+        line3: submission.line3,
+        total_votes: submission.votes.size
+      }
+    end
+
+    render json: submissions
+  end
+
   def toggle_discard
     submission = Submission.find(params[:id])
 
